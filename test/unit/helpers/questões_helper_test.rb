be6b548ace2a4524bd0b34c75d21ea66bd8ce648
direx <diogo.vecchiati@gmail.com>
2012-10-18 22:40:56 -0300

    add gitignore

---
+++ test/unit/helpers/questões_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class QuestõesHelperTest < ActionView::TestCase
+end
---
+++ test/unit/questõe_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class QuestõeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
